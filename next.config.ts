16f214513c6689d97a03fe67bbbd51a72af569bd
fortechzpvt <fortechzpvt@gmail.com>
2026-06-30 22:34:28 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/FORTECHZ-website",
-  assetPrefix: "/FORTECHZ-website/",
   reactStrictMode: true,
   transpilePackages: ["three"],
   images: {
     unoptimized: true,
   },
-  // Add this block right here to bake the backend URL into your static code
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "https://fortechz-website.onrender.com",
   },
